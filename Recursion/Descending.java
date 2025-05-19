6a9fd939b80174f07c9bc37e0f91094f5887bf9c
Shubham Prakash <prakashshubham26@gmail.com>
2025-05-19 14:08:52 +0530

    Recursrion 2

--- Recursion/Descending.java
+++ Recursion/Descending.java
@@ -10,7 +10,18 @@ public class Descending {
         System.out.print(n + " ");
         printDescending( n -1);
     }
+    public static void printAscending(int n) {
+        if (n==1){
+            System.out.print(n + "");
+            return;
+        }
+        printAscending(n-1);
+        System.out.print(n + " ");
+        
+    }
     public static void main(String[] args) {
         printDescending(10);
+        System.out.println();
+        printAscending(10);
     }
 }
---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+    public static int Fibonacci(int n) {
+        if (n==0 || n==1){
+            return n;
+        }
+        int fibnm1 = Fibonacci(n-1);
+        int fibnm2 = Fibonacci(n-2);
+        int fibn = fibnm1 + fibnm2;
+        return fibn;
+    }
+
+    public static void main(String[] args) {
+        System.err.println(Fibonacci(10));
+    }
+}
